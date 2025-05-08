ef925c67aecf2b475b00d438fd1049bc17e44ba6
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-05-08 12:51:01 -0300

    objetos e interfaces

--- fundamentos/src/index.ts
+++ fundamentos/src/index.ts
@@ -1,7 +1,25 @@
-let b: string = 'b';
-let c: number = 1;
-let x: boolean = true;
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao?: string
+}
 
-let m: any = 2;
-m = 'Tugao';
-m = false
+const pessoa: Pessoa = {
+    nome: 'tugão',
+    idade: 21
+}
+
+const pessoa2: Pessoa = {
+    nome: 'Kaio',
+    idade: 31,
+    profissao: 'QA'
+}
+
+const arrayPessoa: Array<Pessoa> = [
+    pessoa,
+    pessoa2,
+]
+
+const arrayNum: number[] = [
+    1, 2, 3, 4
+]
