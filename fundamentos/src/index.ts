54921e305395b493c3e5c7cbba909ce18c1fad8d
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-05-30 14:02:38 -0300

    while

--- fundamentos/src/index.ts
+++ fundamentos/src/index.ts
@@ -1,25 +1,14 @@
-const num: number = 15;
+// for
+/*
+for (let index = 0; index < 5; index++) {
+    console.log(index);
+}
+*/
 
-if(num > 15) {
-    console.log('Num é maior que 15.');
-} else if(num === 15) {
-    console.log('Num é 15.');
-} else {
-    console.log('Num é menor que 15.');
-};
+// while
+let n = 2;
 
-const typeUser = {
-    admin: 'Seja bem-vindo admin.',
-    student: 'Você é um estudante.',
-    viewer: 'Você pode visualizar'
-};
-
-function validateUser(user: string) {
-    console.log(typeUser[user as keyof typeof typeUser]);
-};
-
-const usuario = 'admin';
-
-validateUser(usuario);
-validateUser('student');
-validateUser('viewer');
+while (n < 7) {
+    n +=1;
+    console.log(n);
+}
