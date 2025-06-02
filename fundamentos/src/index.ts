38733eb85aad210b85dca920887b6b36ea7b2486
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-06-02 11:06:08 -0300

    Update index.ts

--- fundamentos/src/index.ts
+++ fundamentos/src/index.ts
@@ -1,14 +1,17 @@
-// for
-/*
-for (let index = 0; index < 5; index++) {
-    console.log(index);
-}
-*/
+const lista: Array<number> = [1, 2, 3, 4];
 
-// while
-let n = 2;
+//const listaString: string[] = ['tulio', 'lady', 'bruna'];
+//const buscaNum = lista.find(num =>num > 2);
+//console.log(buscaNum);
 
-while (n < 7) {
-    n +=1;
-    console.log(n);
-}
+// lista.forEach(num => {
+//     if (num > 2) {
+//         console.log(num);
+//     }
+// })
+
+lista.map(num => {
+    if (num > 2) {
+        console.log(num);
+    }
+})
