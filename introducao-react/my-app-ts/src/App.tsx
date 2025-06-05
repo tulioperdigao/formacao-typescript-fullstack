066d39328323d4b7262e804ae3fd022ae34b1a8d
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2025-06-05 15:01:52 -0300

    primeiros passos com react

---
+++ introducao-react/my-app-ts/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Card} from './components/card'
+
+function App() {
+  return (
+    <>
+      <div>
+        Hello World
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div>
+        Footer
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ introducao-react/my-app-ts/src/components/card.tsx
@@ -0,0 +1,9 @@
+export const Card = () => {
+    return (
+        <div>
+            <h1>Card</h1>
+            <p>Parágrafo</p>
+            <p>Details about card</p>
+        </div>
+    )
+}
